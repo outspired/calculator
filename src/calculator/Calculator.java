677ba09ed947a277e9bcdaf5f159680fd9b036ab
putspired <outspired@gmail.com>
2018-08-28 18:52:36 +0300

    calc ver 2

--- src/calculator/Calculator.java
+++ src/calculator/Calculator.java
@@ -8,112 +8,31 @@ package calculator;
 class Calculator {
 
     String calculate(String[] expression) {
-        //System.out.println(expression[0]);
-        //System.out.println(expression[1]);
-        //System.out.println(expression[2]);
-        //System.out.println(expression[3]);
-        //System.out.println(expression[4]);
-        double a = Double.parseDouble(expression[0]);
-        double b = Double.parseDouble(expression[2]);
-        double result = 0;
-        if (expression.length == 5) {
-            double c = Double.parseDouble(expression[4]);
-            switch (expression[1]) {
+        double result = Double.parseDouble(expression[0]);
+        for (int i = 1; i < expression.length; i += 2) {
+            String op = expression[i];
+            double b = Double.parseDouble(expression[i + 1]);
+            switch (op) {
                 case "+":
-                    switch (expression[3]) {
-                        case "+":
-                            result = a + b + c;
-                            break;
-                        case "-":
-                            result = a + b - c;
-                            break;
-                        case "/":
-                            result = a + b / c;
-                            break;
-                        case "*":
-                            result = a + b * c;
-                            break;
-                        default:
-                            return "ERROR";
-                    }
+                    result += b;
                     break;
                 case "-":
-                    switch (expression[3]) {
-                        case "+":
-                            result = a - b + c;
-                            break;
-                        case "-":
-                            result = a - b - c;
-                            break;
-                        case "/":
-                            result = a - b / c;
-                            break;
-                        case "*":
-                            result = a - b * c;
-                            break;
-                        default:
-                            return "ERROR";
-                    }
+                    result -= b;
                     break;
+                case "*":
+                    result *= b;
+                    break;
+                case "/":
+                    result /= b;
+                    break;
+                default:
+                    return "ERROR";
 
-                case "/":
-                    switch (expression[3]) {
-                        case "+":
-                            result = a / b + c;
-                            break;
-                        case "-":
-                            result = a / b - c;
-                            break;
-                        case "/":
-                            result = a / b / c;
-                            break;
-                        case "*":
-                            result = a / b * c;
-                            break;
-                        default:
-                            return "ERROR";
-                    }
-                    break;
-                case "*":
-                    switch (expression[3]) {
-                        case "+":
-                            result = a * b + c;
-                            break;
-                        case "-":
-                            result = a * b - c;
-                            break;
-                        case "/":
-                            result = a * b / c;
-                            break;
-                        case "*":
-                            result = a * b * c;
-                            break;
-                        default:
-                            return "ERROR";
-                    }
-                    break;
-                default:
-                    return "ERROR";
-            }
-        } else {
-            switch (expression[1]) {
-                case "+":
-                    result = a + b;
-                    break;
-                case "-":
-                    result = a - b;
-                    break;
-                case "/":
-                    result = a / b;
-                    break;
-                case "*":
-                    result = a * b;
-                    break;
-                default:
-                    return "ERROR";
             }
         }
+
         return String.valueOf(result);
+
     }
 
 }
